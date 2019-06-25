adf2b33a3f44d6fb33b08a3096cf2d35c59c8b27
Thabang Gideon Magaola <gideon877@live.com>
2019-06-25 15:10:42 +0200

    implementation

--- phonebook.js
+++ phonebook.js
@@ -1,3 +1,25 @@
 function PhoneBook() {
-	
+    let userList = []
+    const addNumber = (name, number) => {
+        if(name && number) {
+            userList.push({ name, number})
+        }
+    }
+    
+    const getNumber = (name) => {
+        const user = userList.find((userObj) => {
+            return userObj.name == name;
+        })
+        return user.number
+    }
+
+    const getNumbers = () => {
+        return userList;
+    }
+
+    return {
+        addNumber,
+        getNumber,
+        getNumbers
+    }
 }
