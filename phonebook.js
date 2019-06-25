9f444acb2eeeafde5b898d9aeb2282f57d484fb1
coder <makhosandile@projectcodex.co>
2019-06-25 16:43:50 +0200

    added a factory function

--- phonebook.js
+++ phonebook.js
@@ -1,3 +1,25 @@
 function PhoneBook() {
-	
+    const userList = [];
+
+    function addNumber(name, number) {
+        if (name || number) {
+            userList.push({name, number});
+        }
+    }
+
+    function getNumber(name) {
+          return userList.find((obj) => {
+              return obj.name == name;
+          }).number;         
+    }
+
+    function getNumbers() {
+        return userList;
+    }
+
+    return {
+        addNumber,
+        getNumber,
+        getNumbers
+    }
 }
